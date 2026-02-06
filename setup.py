a248d4610edf2f3f2bd374a112ddd9f49d258c4f
rakeshBr123rakesh <rakeshbr2003@gmail.com>
2026-02-06 11:56:05 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+ 
+setup( 
+    name='End-to-End-ML-project', 
+    version='0.0.1', 
+    author='Rocky', 
+    author_email='rakeshbr2003@gmail.com', 
+    packages=find_packages(), 
+    install_requires=[], 
+) 
